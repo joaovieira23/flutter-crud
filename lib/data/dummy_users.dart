be291b4a7a780b1698a9b8a10341f2e6725b1440
Joao Andrade <joaovictorvieira.23@hotmail.com>
2022-05-20 08:40:09 -0500

    feat: define initial structure

---
+++ lib/data/dummy_users.dart
@@ -0,0 +1,30 @@
+// ignore_for_file: unnecessary_const
+
+import 'package:flutter_crud/models/user.dart';
+
+const DUMMY_USERS = {
+  '1': const User(
+    id: '1',
+    name: 'Maria',
+    email: 'maria@alunos.com.br',
+    avatarUrl: 'https://cdn.pixabay.com/photo/2016/09/01/08/24/smiley-1635449_960_720.png'
+  ),
+  '2': const User(
+    id: '2',
+    name: 'João',
+    email: 'joaovictorvieira.23@hotmail.com',
+    avatarUrl: 'https://cdn.pixabay.com/photo/2016/03/31/20/31/amazed-1295833_960_720.png'
+  ),
+  '3': const User(
+    id: '3',
+    name: 'Fernanda',
+    email: 'fernanda@zymail.com.br',
+    avatarUrl: 'https://cdn.pixabay.com/photo/2019/03/21/20/29/eyewear-4071870_960_720.jpg'
+  ),
+  '4': const User(
+    id: '4',
+    name: 'Amanda',
+    email: 'amanda@gmail.com.br',
+    avatarUrl: 'https://cdn.pixabay.com/photo/2021/01/17/09/11/woman-5924366_960_720.jpg'
+  )
+};
